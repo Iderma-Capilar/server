62bde5315613e2ee520e5ef44fbb75d5521bac30
psamirt <p_samir@hotmail.com>
2024-09-13 15:05:46 -0500

    hotfix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import categoryRoutes from "./categoryRoutes.js";
-import productRoutes from "./productRoutes.js";
-import doctorRoutes from "../routes/doctorRoutes.js";
 import technologyRoutes from "../routes/technologyRouter.js";
+import serviceRoutes from "../routes/serviceRouter.js";
+
 const router = Router();
 
 router.use("/categories", categoryRoutes);
-router.use("/products", productRoutes);
-router.use("/doctorRoutes", doctorRoutes);
 router.use("/technology", technologyRoutes);
+router.use("services", serviceRoutes);
 
 export default router;
---
+++ src/routes/serviceRouter.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  createService,
+  deleteService,
+  getAllServices,
+  getServiceById,
+  updateService,
+} from "../controller/serviceControllers.js";
+
+const router = Router();
+
+router.post("/", createService);
+router.get("/", getAllServices);
+router.get("/:id", getServiceById);
+router.put("/:id", updateService);
+router.delete("/:id", deleteService);
+
+export default router;
