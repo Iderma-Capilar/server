39fd46da475a52d23802162a427fd0b0638f9a55
EmanuelCG <mr.emanuelcg@gmail.com>
2024-09-14 09:47:19 -0500

    testimonials schema

--- src/database/models/testimonials.js
+++ src/database/models/testimonials.js
@@ -2,32 +2,48 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../index.js";
 
 const Testimonials = sequelize.define(
-  "TestiminialComments",
+  "Testiminials",
   {
-    tcomment_id: {
+    id_testimonial: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    type_testimonial: {
+        type: DataTypes.STRING, //comment o video
+        allowNull: false,
     },
     comment: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
-    thumbnail: {
+    thumbnail_username: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+    },
+    thumbnail_video: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    video_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    btn_link: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    vide_description: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
   },
   {
-    tableName: "testimonial_comments",
+    tableName: "testimonials",
   }
 );
 
