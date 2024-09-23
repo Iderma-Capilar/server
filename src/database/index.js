b966a26113b4d9fba11812e13dd817c4872a529f
psamirt <p_samir@hotmail.com>
2024-09-23 10:14:12 -0500

    entorno de desarrollo

--- src/database/index.js
+++ src/database/index.js
@@ -4,21 +4,32 @@ dotenv.config();
 
 const { DATABASE_URL } = process.env;
 
+// ENTORNO DE PRODUCCION
+// -----------------------------------
+
+// const sequelize = new Sequelize(DATABASE_URL, {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+
+
+// ENTORNO DE LOCAL DE DESARROLLO
+// -----------------------------------
+
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
 });
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connected to database");
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: true });
     console.log("All models were synchronized successfully.");
   } catch (err) {
     console.error("Error connecting to the database:", err);
